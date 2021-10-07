bbd284bfe2228be915f71a61c4daea2f6b8d73ac
root <root@localhost>
2021-10-07 11:40:47 +0530

    auto publish 1633587046946

---
+++ src/main.py
@@ -0,0 +1,11 @@
+A=input("do you want to start hacking press y for yes")
+if 'y' in A:
+		print(10*"hacking in progress| 1%| completed\n")
+		print(10*("hacking in progress |23%| completed\n"))
+		print(10*("hacking in progress |50%| completed\n"))
+		print(10*("hacking in progress |79%| completed\n"))
+		print(10*("hacking completed |100%|\n"))
+B=input("do you want to acess folder type y for yes")
+if 'y' in B:
+		print(10*"access granted\n")
+		print(10*"you are hacked\n")
